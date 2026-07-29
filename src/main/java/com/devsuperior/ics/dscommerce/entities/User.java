4a8897b0781b3e7f392b2fb51f03a7338b424b3b
icsmv001 <icsvm001@gmail.com>
2026-07-28 23:14:47 -0300

    configurando banco H2 e entidade USER, primeira versao

---
+++ src/main/java/com/devsuperior/ics/dscommerce/entities/User.java
@@ -0,0 +1,107 @@
+package com.devsuperior.ics.dscommerce.entities;
+
+import java.time.LocalDate;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private LocalDate birtDate;
+	private String password;
+	
+
+	public User() {
+		
+	}
+
+
+	public User(Long id, String name, String email, String phone, LocalDate birtDate, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.birtDate = birtDate;
+		this.password = password;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+	public LocalDate getBirtDate() {
+		return birtDate;
+	}
+
+
+	public void setBirtDate(LocalDate birtDate) {
+		this.birtDate = birtDate;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+	
+}
