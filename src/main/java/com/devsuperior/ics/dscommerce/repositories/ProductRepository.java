ccde3562467afa28c5a46b789ac2d384bc411a1e
icsmv001 <icsvm001@gmail.com>
2026-08-30 21:00:43 -0300

    premeiro Teste com Repository - sucesso

---
+++ src/main/java/com/devsuperior/ics/dscommerce/repositories/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.devsuperior.ics.dscommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.ics.dscommerce.entities.Product;
+
+public interface ProductRepository extends JpaRepository <Product, Long>{
+	
+	
+}
+
+
+	
+	
+
