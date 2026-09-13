1cc81b863a2e47e9ee5a65a0185d1e07e31db305
icsmv001 <icsvm001@gmail.com>
2026-09-13 14:21:28 -0300

    customizando a resposta da validacao

--- src/main/java/com/devsuperior/ics/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/ics/dscommerce/controllers/ProductController.java
@@ -20,6 +20,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.devsuperior.ics.dscommerce.dto.ProductDTO;
 import com.devsuperior.ics.dscommerce.services.ProductService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping(value= "/products")
 public class ProductController {
@@ -44,7 +46,7 @@ public class ProductController {
 	
 		
 	@PostMapping
-	public ResponseEntity<ProductDTO> insert( @RequestBody  ProductDTO dto) {
+	public ResponseEntity<ProductDTO> insert(@Valid  @RequestBody  ProductDTO dto) {
 		 dto =  service.insert(dto);
 		 
          URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
@@ -55,7 +57,7 @@ public class ProductController {
 	
 	
 	@PutMapping(value="/{id}")
-	public ResponseEntity<ProductDTO> update( @PathVariable Long id,  @RequestBody  ProductDTO dto ) {
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid  @RequestBody  ProductDTO dto ) {
 	
 	dto = service.update(id, dto);
 	return ResponseEntity.ok(dto);
